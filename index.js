4ae6a37a0e94d66bb93722ab0bac8aecced52d14
Ashraf Manna <optimisticashraf@gmail.com>
2025-03-23 01:23:14 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app=express();
 const port= process.env.PORT || 5000
@@ -9,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://BistroBoss:<db_password>@cluster0.pqwog.mongodb.net/?appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
